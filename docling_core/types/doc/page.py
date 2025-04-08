fix: Better `BoundingRectangle.angle` and `BoundingRectangle.angle_360` computation (#237)
* fix: fix `BoundingRectanble.angle` and `BoundingRectanble.angle_360` computation

Signed-off-by: Clément Doumouro <clement.doumouro@gmail.com>

* fix: compute page bounding rectangle angle between (0-2pi) / (0-360)

Signed-off-by: Clément Doumouro <clement.doumouro@gmail.com>

---------

Signed-off-by: Clément Doumouro <clement.doumouro@gmail.com>
Co-authored-by: Christoph Auer <60343111+cau-git@users.noreply.github.com>

docling_core/types/doc/page.py
@@ -116,33 +116,28 @@ class BoundingRectangle(BaseModel):
 
     @property
     def angle(self) -> float:
-        """Calculate the angle of the rectangle in radians."""
+        """Calculate the angle of the rectangle in radians (0-2pi range)."""
         p_0 = ((self.r_x0 + self.r_x3) / 2.0, (self.r_y0 + self.r_y3) / 2.0)
         p_1 = ((self.r_x1 + self.r_x2) / 2.0, (self.r_y1 + self.r_y2) / 2.0)
 
         delta_x, delta_y = p_1[0] - p_0[0], p_1[1] - p_0[1]
 
-        if abs(delta_x) > 1.0e-3:
-            return math.atan(delta_y / delta_x)
-        elif delta_y > 0:
-            return 3.142592 / 2.0
+        if abs(delta_y) < 1.0e-3:
+            angle = 0.0
+        elif abs(delta_x) < 1.0e-3:
+            angle = np.pi / 2.0 if delta_y > 0 else -np.pi / 2.0
         else:
-            return -3.142592 / 2.0
+            angle = math.atan(delta_y / delta_x)
+        if delta_x < 0:
+            angle += np.pi
+        if angle < 0:
+            angle += 2 * np.pi
+        return angle
 
     @property
     def angle_360(self) -> int:
         """Calculate the angle of the rectangle in degrees (0-360 range)."""
-        p_0 = ((self.r_x0 + self.r_x3) / 2.0, (self.r_y0 + self.r_y3) / 2.0)
-        p_1 = ((self.r_x1 + self.r_x2) / 2.0, (self.r_y1 + self.r_y2) / 2.0)
-
-        delta_x, delta_y = p_1[0] - p_0[0], p_1[1] - p_0[1]
-
-        if abs(delta_y) < 1.0e-2:
-            return 0
-        elif abs(delta_x) < 1.0e-2:
-            return 90
-        else:
-            return round(-math.atan(delta_y / delta_x) / np.pi * 180)
+        return round(self.angle / np.pi * 180)
 
     @property
     def centre(self):