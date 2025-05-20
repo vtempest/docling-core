ae1cdcedad3e9350daa17df5e71032455bdb8d3b
Said Gürbüz <57208860+Saidgurbuz@users.noreply.github.com>
2025-05-20 15:06:25 +0200

    test: update tests for load_from_doctags (#299)
    * update tests for load_from_doctags
    
    Signed-off-by: Saidgurbuz <said.gurbuz@epfl.ch>
    
    * update tests to fix the flakiness
    
    Signed-off-by: Saidgurbuz <said.gurbuz@epfl.ch>
    
    ---------
    
    Signed-off-by: Saidgurbuz <said.gurbuz@epfl.ch>

--- test/test_doctags_load.py
+++ test/test_doctags_load.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 from PIL import Image as PILImage
@@ -5,63 +6,114 @@ from PIL import Image as PILImage
 from docling_core.types.doc import DoclingDocument
 from docling_core.types.doc.document import DocTagsDocument, PictureTabularChartData
 
+from .test_data_gen_flag import GEN_TEST_DATA
+
+
+def verify(exp_file: Path, actual: dict):
+    if GEN_TEST_DATA:
+        with open(exp_file, "w", encoding="utf-8") as f:
+            json.dump(actual, f, indent=2)
+            f.write("\n")
+    else:
+        with open(exp_file, "r", encoding="utf-8") as f:
+            expected = json.load(f)
+
+        # we removed image URIs in both dicts for lossy comparison
+        # as the test was flaky due to URIs
+        def strip_image_uris(d):
+            if isinstance(d, dict):
+                return {
+                    k: strip_image_uris(v)
+                    for k, v in d.items()
+                    if k not in {"uri", "image_uri"}
+                }
+            elif isinstance(d, list):
+                return [strip_image_uris(x) for x in d]
+            else:
+                return d
+
+        expected_stripped = strip_image_uris(expected)
+        actual_stripped = strip_image_uris(actual)
+        assert (
+            expected_stripped == actual_stripped
+        ), "Dicts differ (ignoring image URIs)"
+
+        if "data:image/png;base64" in str(expected):
+            # check if the image URIs are the same
+            assert "data:image/png;base64" in str(actual), "Image URIs does not exist"
+
 
 def test_doctags_load_from_files():
-    doc = DoclingDocument(name="Document")
 
     doctags_doc = DocTagsDocument.from_doctags_and_image_pairs(
         [Path("test/data/doc/page_with_pic.dt")],
         [Path("test/data/doc/page_with_pic.png")],
     )
 
-    doc.load_from_doctags(doctags_doc)
-    # print(doc.export_to_html())
+    doc = DoclingDocument.load_from_doctags(doctags_doc)
+    exp = "test/data/doc/page_with_pic_from_files.dt.json"
+    verify(
+        exp_file=exp,
+        actual=doc.export_to_dict(),
+    )
 
 
 def test_doctags_load_from_memory():
-    doc = DoclingDocument(name="Document")
 
     doctags = Path("test/data/doc/page_with_pic.dt").open("r").read()
     image = PILImage.open(Path("test/data/doc/page_with_pic.png"))
 
     doctags_doc = DocTagsDocument.from_doctags_and_image_pairs([doctags], [image])
 
-    doc.load_from_doctags(doctags_doc)
-    # print(doc.export_to_html())
+    doc = DoclingDocument.load_from_doctags(doctags_doc)
+
+    exp = "test/data/doc/page_with_pic.dt.json"
+    verify(
+        exp_file=exp,
+        actual=doc.export_to_dict(),
+    )
 
 
 def test_doctags_load_without_image():
-    doc = DoclingDocument(name="Document")
     doctags = Path("test/data/doc/page_with_pic.dt").open("r").read()
     doctags_doc = DocTagsDocument.from_doctags_and_image_pairs([doctags], None)
-    doc.load_from_doctags(doctags_doc)
-    # print(doc.export_to_html())
+    doc = DoclingDocument.load_from_doctags(doctags_doc)
+    exp = "test/data/doc/page_without_pic.dt.json"
+    verify(
+        exp_file=exp,
+        actual=doc.export_to_dict(),
+    )
 
 
 def test_doctags_load_for_kv_region():
-    doc = DoclingDocument(name="Document")
     doctags = Path("test/data/doc/doc_with_kv.dt").open("r").read()
     image = PILImage.open(Path("test/data/doc/doc_with_kv.png"))
     doctags_doc = DocTagsDocument.from_doctags_and_image_pairs([doctags], [image])
-    doc.load_from_doctags(doctags_doc)
-    # print(doc.export_to_html())
+    doc = DoclingDocument.load_from_doctags(doctags_doc)
+    exp = "test/data/doc/doc_with_kv.dt.json"
+    verify(
+        exp_file=exp,
+        actual=doc.export_to_dict(),
+    )
 
 
 def test_multipage_doctags_load():
-    doc = DoclingDocument(name="Document")
     doctags = Path("test/data/doc/2206.01062.yaml.dt").open("r").read()
     doctags_doc = DocTagsDocument.from_multipage_doctags_and_images(doctags, None)
-    doc.load_from_doctags(doctags_doc)
-    # print(doc.export_to_html())
+    doc = DoclingDocument.load_from_doctags(doctags_doc)
+    exp = "test/data/doc/2206.01062.yaml.dt.json"
+    verify(
+        exp_file=exp,
+        actual=doc.export_to_dict(),
+    )
 
 
 def test_doctags_chart():
-    doc = DoclingDocument(name="Document")
     doctags_doc = DocTagsDocument.from_doctags_and_image_pairs(
         [Path("test/data/doc/barchart.dt")],
         [Path("test/data/doc/barchart.png")],
     )
-    doc.load_from_doctags(doctags_doc)
+    doc = DoclingDocument.load_from_doctags(doctags_doc)
     for pic in doc.pictures:
         tabular_chart_annotations = [
             ann for ann in pic.annotations if isinstance(ann, PictureTabularChartData)
@@ -70,24 +122,22 @@ def test_doctags_chart():
 
 
 def test_doctags_table_provenances_and_captions():
-    doc = DoclingDocument(name="Document")
     doctags_doc = DocTagsDocument.from_doctags_and_image_pairs(
         [Path("test/data/doc/01030000000083.dt")],
         [Path("test/data/doc/01030000000083.png")],
     )
-    doc.load_from_doctags(doctags_doc)
+    doc = DoclingDocument.load_from_doctags(doctags_doc)
     for table in doc.tables:
         assert len(table.prov) > 0
         assert len(table.captions) > 0
 
 
 def test_doctags_picture_provenances_and_captions():
-    doc = DoclingDocument(name="Document")
     doctags_doc = DocTagsDocument.from_doctags_and_image_pairs(
         [Path("test/data/doc/01030000000111.dt")],
         [Path("test/data/doc/01030000000111.png")],
     )
-    doc.load_from_doctags(doctags_doc)
+    doc = DoclingDocument.load_from_doctags(doctags_doc)
     for picture in doc.pictures:
         assert len(picture.prov) > 0
         assert len(picture.captions) > 0
